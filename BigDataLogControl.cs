bfcbacff37b0b218623047e09093cd58b0ed06b6
Jerome Biotidara <jeromebiotidara@gmail.com>
2017-12-28 12:27:31 +0000

    init commit

---
+++ BigDataLogControl.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigDataTiler
+{
+    public class BigDataLogControl
+    {
+        private static bool isLocal = Environment.GetEnvironmentVariable("TILER_ENV") == "local";
+        private static string EndpointUrl = isLocal ? "https://tilerbigdata0.documents.azure.com:443/":"https://tilerbigdata.documents.azure.com:443/";
+        private static string dbName = "TilerBigDataDB";
+        private static string collectionName = "UserLogs";
+        private static string partitionKey = "/UserId";
+        private static int OfferThroughput = 50000;
+        private string PrimaryKey = isLocal ? ConfigurationManager.AppSettings["azureDocumentDbKeyLocal"] :ConfigurationManager.AppSettings["azureDocumentDbKey"];
+        private DocumentClient Client;
+        public LogChange loadedDocument { get; set; }
+        public BigDataLogControl()
+        {
+            Client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+        }
+
+        public async Task createAzureDocumentDatabase()
+        {
+            await BigDataLogControl.createAzureDocumentDatabase(EndpointUrl, PrimaryKey).ConfigureAwait(false);
+        }
+        public static async Task createAzureDocumentDatabase(string EndpointUrl, string PrimaryKey)
+        {
+            DocumentClient client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+            Database dataBase = new Database { Id = dbName };
+            await client.CreateDatabaseIfNotExistsAsync(dataBase).ConfigureAwait(false);
+            DocumentCollection myCollection = new DocumentCollection();
+            myCollection.Id = collectionName;
+            myCollection.PartitionKey.Paths.Add(partitionKey);
+            await client.CreateDocumentCollectionIfNotExistsAsync(
+                UriFactory.CreateDatabaseUri(dbName),
+                myCollection,
+                new RequestOptions { OfferThroughput = OfferThroughput }).ConfigureAwait(false);
+        }
+        public async Task AddLogDocument(LogChange log)
+        {
+            await Client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(dbName, collectionName), log).ConfigureAwait(false);
+        }
+
+        async Task<Document> loadDocument(string userId, string documentId)
+        {
+            RequestOptions options = new RequestOptions { PartitionKey = new PartitionKey(userId) };
+            Uri uri = UriFactory.CreateDocumentUri(dbName, collectionName, documentId);
+            Document retrievedDocument = await Client.ReadDocumentAsync(uri, options).ConfigureAwait(false);
+            return retrievedDocument;
+        }
+
+        public async Task<LogChange> getDocument(string userId, string documentId)
+        {
+            Document loadedDocument = await loadDocument(userId, documentId).ConfigureAwait(false);
+            byte[] allBytesbefore = loadedDocument.GetPropertyValue<byte[]>("ZippedLog");
+            LogChange retValue = new LogChange();
+            retValue.ZippedLog = allBytesbefore;
+            retValue.Id = loadedDocument.GetPropertyValue<string>("Id");
+            retValue.UserId = loadedDocument.GetPropertyValue<string>("UserId");
+            retValue.TypeOfEvent = loadedDocument.GetPropertyValue<string>("TypeOfEvent");
+            retValue.TimeOfCreation = loadedDocument.GetPropertyValue<DateTimeOffset>("TimeOfCreation");
+            retValue.JsTimeOfCreation = loadedDocument.GetPropertyValue<ulong>("JsTimeOfCreation");
+            this.loadedDocument = retValue;
+            return retValue;
+        }
+
+        public async Task writeDocumentTofile(byte[] bytes, string fullZipPathName)
+        {
+            byte[] allBytesbefore = bytes;
+
+            string documentZipPathCopy = @fullZipPathName;
+            FileStream fs = new FileStream(documentZipPathCopy, FileMode.Create);
+            fs.Write(allBytesbefore, 0, allBytesbefore.Length);
+        }
+    }
+}
---
+++ LogChange.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using TilerElements;
+
+namespace BigDataTiler
+{
+    public class LogChange
+    {
+        [JsonProperty("UserId")]
+        public string UserId { get; set; }
+        [JsonProperty("id")]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [JsonProperty("TypeOfEvent")]
+        public string TypeOfEvent { get; set; }
+        [JsonConverter(typeof(IsoDateTimeConverter))]
+        [JsonProperty("TimeOfCreation")]
+        public DateTimeOffset TimeOfCreation { get; set; }
+
+        [JsonProperty("JsTimeOfCreation")]
+        public ulong JsTimeOfCreation { get; set; }
+        [JsonProperty("ZippedLog")]
+        public byte[] ZippedLog { get; set; }
+        public void loadXmlFile(XmlDocument xmlDoc)
+        {
+            byte[] retValue = null;
+            MemoryStream ms;
+
+            //MemoryStream textFileStream = new MemoryStream();
+            
+
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                    {
+                        var demoFile = archive.CreateEntry(this.TimeOfCreation.toJSMilliseconds().ToString() + ".xml", CompressionLevel.Optimal);
+
+                        using (var entryStream = demoFile.Open())
+                        {
+                            //textFileStream.CopyTo(entryStream);
+                            xmlDoc.Save(entryStream);
+                        }
+                    }
+
+                    retValue = memoryStream.ToArray();
+                }
+            }
+            ZippedLog = retValue;
+        }
+    }
+}
